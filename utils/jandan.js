cadca3d2a3bb95cfd7d9216bd437662033e63ecb
qiuziz <qiuzhi.zhu@shuyun.com>
2017-12-07 18:10:38 +0800

    proxy

--- utils/jandan.js
+++ utils/jandan.js
@@ -2,7 +2,7 @@
  * @Author: qiuziz
  * @Date: 2017-05-17 20:12:03
  * @Last Modified by: qiuziz
- * @Last Modified time: 2017-12-07 17:48:39
+ * @Last Modified time: 2017-12-07 18:10:30
  */
 
 var http = require('http'),
@@ -29,7 +29,7 @@ function random(m, n) {
 
 function jandan(url) {
 	sleep.sleep(10);
-	phantom.create().then(function(ph) {
+	phantom.create('--proxy=106.4.134.101:9000').then(function(ph) {
 		ph.createPage().then(function(page) {
 			if (!page.settings) return;
 			page.settings.userAgent = USER_AGENTS[random(0, LEN)];
