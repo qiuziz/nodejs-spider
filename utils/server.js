0f6e8163514a2a87633b77cbaf5d472642cc9ec4
qiuz <zhu_anly@163.com>
2018-09-28 09:55:18 +0800

    spider package

---
+++ utils/server.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: qiuz
+ * @Date: 2017-05-11 15:44:24
+ */
+
+const http = require('http'),
+		express = require('express'),
+		jandan = require('./jandan.js');
+
+const app = express();
+
+const pageUrl = 'https://jandan.net/ooxx';
+const port = process.env.PORT || 4200;
+
+jandan(pageUrl);
+
+app.use('/jandan/images', require('./jandan-api.js'));
+
+http.createServer(app).listen(port);
+
+
