ca5bb4ee8293af1455d6fa5f36fcf7c788e810c6
qiuz <zhu_anly@163.com>
2019-10-22 21:14:00 +0800

    update

--- docker_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker-compose down
-
-docker rmi registry.cn-shanghai.aliyuncs.com/qiuz/photo-web:laster
-docker rmi registry.cn-shanghai.aliyuncs.com/qiuz/photo-node:laster
-
-docker-compose up -d
