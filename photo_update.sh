62ad96d7f281a7d1cd8f2b7db9fa07c8648cfb43
qiuz <zhu_anly@163.com>
2019-10-23 10:16:31 +0800

    photo_update

--- photo_update.sh
+++ photo_update.sh
@@ -2,7 +2,11 @@
 
 docker-compose down
 
-docker rmi registry.cn-shanghai.aliyuncs.com/qiuz/photo-web:laster
-docker rmi registry.cn-shanghai.aliyuncs.com/qiuz/photo-node:laster
+docker rmi registry.cn-shanghai.aliyuncs.com/qiuz/photo-web:latest
+docker rmi registry.cn-shanghai.aliyuncs.com/qiuz/photo-node:latest
+
+docker-compose up -d
+
+docker cp db.js photo-node:/app/src
 
 docker-compose up -d
